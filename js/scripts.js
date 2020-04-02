23eb7dd6cb4095fef867dc75f44f5d3c82e5664b
lillapulay <59831508+lillapulay@users.noreply.github.com>
2020-04-02 21:48:25 +0200

    Added Pokémons

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
-alert('Hello World!');
-
-var favoriteFood = 'Pizza';
-document.write(favoriteFood);
+var repository = [
+{
+  pokemonName : 'Phanpy',
+  pokemonHeight : 0.5,
+  pokemonAbilities : ['Sand-veil', 'Pickup'],
+  pokemonTypes : ['Ground'],
+},
+{
+  pokemonName : 'Crustle',
+  pokemonHeight : 1.4,
+  pokemonAbilities : ['Sturdy', 'Shell-armor', 'Weak-armor'],
+  pokemonTypes : ['Bug', 'Rock'],
+},
+{
+  pokemonName : 'Rhyhorn',
+  pokemonHeight : 1.00,
+  pokemonAbilities : ['Lightningrod', 'Rock-head', 'Reckless'],
+  pokemonTypes : ['Rock', 'Ground'],
+},
+{
+  pokemonName : 'Psyduck',
+  pokemonHeight : 0.8,
+  pokemonAbilities : ['Damp', 'Cloud-nine', 'Swift-swim'],
+  pokemonTypes : ['Water'],
+}
+]
+console.log(repository)
