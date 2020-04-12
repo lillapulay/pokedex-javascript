1d78c934cba7f32beb1aa27a3f7671d025faa0d5
lillapulay <59831508+lillapulay@users.noreply.github.com>
2020-04-12 12:10:08 +0200

    Added FP principles

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,55 @@
-var pokemonList = [
-  {
-    name : 'Phanpy',
-    height : 0.5,
-    abilities : ['Sand-veil', ' Pickup'],
-    types : ['Ground'],
-  },
-  {
-    name : 'Crustle',
-    height : 1.4,
-    abilities : ['Sturdy', ' Shell-armor', ' Weak-armor'],
-    types : ['Bug', ' Rock'],
-  },
-  {
-    name : 'Rhyhorn',
-    height : 1.00,
-    abilities : ['Lightningrod', ' Rock-head', ' Reckless'],
-    types : ['Rock', ' Ground'],
-  },
-  {
-    name : 'Psyduck',
-    height : 0.8,
-    abilities : ['Damp', ' Cloud-nine', ' Swift-swim'],
-    types : ['Water'],
+//IIFE wrap
+var pokemonRepository = (function () {
+  var repository = [ // original repository moved into the wrap - no changes made
+    {
+      name : 'Phanpy',
+      height : 0.5,
+      abilities : ['Sand-veil', ' Pickup'],
+      types : ['Ground'],
+    },
+    {
+      name : 'Crustle',
+      height : 1.4,
+      abilities : ['Sturdy', ' Shell-armor', ' Weak-armor'],
+      types : ['Bug', ' Rock'],
+    },
+    {
+      name : 'Rhyhorn',
+      height : 1.00,
+      abilities : ['Lightningrod', ' Rock-head', ' Reckless'],
+      types : ['Rock', ' Ground'],
+    },
+    {
+      name : 'Psyduck',
+      height : 0.8,
+      abilities : ['Damp', ' Cloud-nine', ' Swift-swim'],
+      types : ['Water'],
+    }
+  ]; // end of repository
+
+
+// defining public functions separately
+  function getAll() {
+    return repository;
   }
-];
 
-pokemonList.forEach(function(currentPokemon){
-  document.write('<h3>' +  currentPokemon.name + '</h3>');
+  function add(pokemon) {
+    repository.push(pokemon);
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})(); // end of IIFE
+
+function myLoopFunction(pokemon){
+  document.write('<h3>' +  pokemon.name + '</h3>');
   document.write('<p>'
-    + ' Height: ' +  currentPokemon.height + '<br>'
-    + ' Abilities: ' +  currentPokemon.abilities + '<br>'
-    + ' Type: ' +  currentPokemon.types +
+    + ' Height: ' +  pokemon.height + '<br>'
+    + ' Abilities: ' +  pokemon.abilities + '<br>'
+    + ' Type: ' +  pokemon.types +
     '</p>');
-});
+}
 
-//working code
-//for (var i = 0; i < pokemonList.length; i++) {
-  //document.write('<h3>' + pokemonList[i].name + '</h3>' + ' Height: ' + pokemonList[i].height)
-//  if (pokemonList[i].height > 1.00) {
-//    document.write(' (Wow that\'s big!)' )
-//  }
-//}
+pokemonRepository.getAll().forEach(myLoopFunction); //not sure how to rewrite the forEach for the IIFE - not working
